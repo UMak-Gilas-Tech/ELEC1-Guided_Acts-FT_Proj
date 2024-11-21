886f76c3403fed232fff1c343a0792a8da091135
Mark Siazon <marka.siazon@gmail.com>
2024-11-21 18:09:03 +0800

    ✅ | Add Practice Exercise 7: Rating Bar and Alert Dialog
        - Implemented GuidedActivitySeven to demonstrate the use of RatingBar and AlertDialog.
        - Added layout file `activity_ge7_rating_bar_alert_dialog.xml` with:
          - Title TextView for exercise description.
          - RatingBar for user interaction.
          - TextView to display rating results dynamically.
          - Buttons for displaying rating feedback and closing the app with a confirmation dialog.
        - Included logic to change text color based on rating value.
        - Added AlertDialog for app close confirmation.

--- app/src/main/java/com/elecone/guidedexercise/GuidedActivitySeven.java
+++ app/src/main/java/com/elecone/guidedexercise/GuidedActivitySeven.java
@@ -1,6 +1,113 @@
 package com.elecone.guidedexercise;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class GuidedActivitySeven extends Activity {
+public class GuidedActivitySeven extends BaseActivity {
+    RatingBar ratingBar;
+    TextView rate;
+    Button click, close;
+    AlertDialog.Builder alertDialogBuilder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ge7_rating_bar_alert_dialog);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("RatingBar and AlertDialog");
+            getSupportActionBar().setHomeAsUpIndicator(android.R.drawable.ic_menu_revert);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        // object creation of alertDialogBuilder
+        alertDialogBuilder = new AlertDialog.Builder(this);
+        ratingBar = findViewById(R.id.ratingBar);
+        rate = findViewById(R.id.tvResultGE7);
+        click = findViewById(R.id.btnClickGE7);
+        close = findViewById(R.id.btnCloseGE7);
+
+        showRating();
+        closeApplication();
+    }
+
+    public void showRating() {
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                if (ratingBar.getRating() >= 3) {
+                    rate.setTextColor(Color.GREEN);
+                } else {
+                    rate.setTextColor(Color.RED);
+                }
+                rate.setText("Rate: " + ratingBar.getRating());
+            }
+        });
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Rate: " + ratingBar.getRating(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void closeApplication() {
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialogBuilder.setTitle("Warning Message!")
+                        .setMessage("Do you want to close the Application?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // once the Yes is selected it will close the application using
+                                // this pre-defined method named finish() in android
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // once the No is selected it will simply close/cancel the
+                                // dialogInterface
+                                dialogInterface.cancel();
+                            }
+                        })
+                        // this will set whether this dialog is cancelable or not, by default is TRUE
+                        .setCancelable(false);
+                // display our dialog builder
+                alertDialogBuilder.show();
+            }
+        });
+    }
+
+    // this is a pre-defined method in android that we can override and put some algorithm and it will be called once the back button is clicked
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        alertDialogBuilder.setTitle("Warning Message!")
+                .setMessage("Do you want to close the Application?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                })
+                .setCancelable(false);
+        alertDialogBuilder.show();
+    }
 }
